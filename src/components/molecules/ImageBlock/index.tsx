c17e9c70e3e1d7c35985917bd2ca2eb0036ac8d9
Pratham Ingale <pratham.ing49@gmail.com>
2025-02-03 12:16:41 -0500

    added image and did stuff

--- src/components/molecules/ImageBlock/index.tsx
+++ src/components/molecules/ImageBlock/index.tsx
@@ -3,19 +3,45 @@ import classNames from 'classnames';
 import { Annotated } from '@/components/Annotated';
 
 export default function ImageBlock(props) {
-    const { elementId, className, url, altText = '' } = props;
+    const { elementId, className, url, altText = '', width, height } = props;
+
     if (!url) {
         return null;
     }
 
     return (
         <Annotated content={props}>
-            <img
-                id={elementId || null}
-                className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', className)}
-                src={url}
-                alt={altText}
-            />
+            <div
+                className={classNames(
+                    'sb-component',
+                    'sb-component-block',
+                    'sb-component-image-block',
+                    className,
+                    'oval-image-container' // Added class for container
+                )}
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    maxWidth: width || '400px', // Default size
+                    maxHeight: height || 'auto',
+                    position: 'relative' // Needed for absolute border
+                }}
+            >
+                <img
+                    id={elementId || null}
+                    src={url}
+                    alt={altText}
+                    className="oval-image" // Add class to target with CSS
+                    style={{
+                        width: width || '100%',
+                        height: height || 'auto',
+                        objectFit: 'cover',
+                        borderRadius: '50% / 60%', // Adjusted for oval shape
+                    }}
+                />
+                <div className="oval-border"></div> {/* Added border */}
+            </div>
         </Annotated>
     );
 }
